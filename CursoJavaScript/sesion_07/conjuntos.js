49dbf2ff5eece32ab530242a291770f5de8c3a98
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2022-07-04 12:14:32 +0200

    ejercicio 1

---
+++ CursoJavaScript/sesion_07/conjuntos.js
@@ -0,0 +1,7 @@
+//Un nuevo Set con los nombres de tu familia
+const família = ["Juan Jose", "Concepción", "José Eduardo", "Ana Maria"]
+const miFamilia = new Set(família)
+//Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+miFamilia.add("José Eduardo");
+//Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+miFamilia.add("Javascript")
