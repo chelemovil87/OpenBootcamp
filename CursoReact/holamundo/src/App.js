7e43e8982341309319f58282366f08a321509370
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2022-07-11 19:12:31 +0200

    Primera modificacion react

--- CursoReact/holamundo/src/App.js
+++ CursoReact/holamundo/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Primera aplicacion de desarrollo de Jose Eduardo
         </p>
         <a
           className="App-link"
