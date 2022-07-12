d0f3b99c0b83ff801b4c04ae4a729d965e9f97e6
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2022-07-12 17:22:25 +0200

    componente funcional react

--- CursoReact/holamundo/src/components/pure/greetingF.jsx
+++ CursoReact/holamundo/src/components/pure/greetingF.jsx
@@ -1,40 +1,29 @@
-import React, {useState} from 'react';
-import PropTypes from 'prop-types';
-
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 const GreetingF = (props) => {
-   //Breve introduccion a useState
+    //Breve introduccion a useState
     // const [variable, metodo para actualizarlo] = useState (valor inicial)
-    const [age, setage] =useState(29)
-   
+    const [age, setage] = useState(29);
+
     const birthday = () => {
         //actualizar el state
         setage(age + 1);
-    }
+    };
 
     return (
         <div>
-            
-                <h1>
-                    Hola {props.name } desde componente funcional!
-                </h1>
-                {/* <h2>
-                    Tu edad es de: {this.state.age }
-                </h2>
-                <div> 
-                    <button onClick={this.birthday}>
-                        Cumplir Años!!
-                    </button>    
-                </div> */}
-            
+            <h1>Hola {props.name} desde componente funcinal!</h1>
+            <h2>Tu edad es de : {age}</h2>
+            <div>
+                <button onClick={birthday}>Cumplir Años!!</button>
+            </div>
         </div>
-    );
+    )
 };
-
 
 GreetingF.propTypes = {
-    name: PropTypes.string,
+  name: PropTypes.string,
 };
-
 
 export default GreetingF;
