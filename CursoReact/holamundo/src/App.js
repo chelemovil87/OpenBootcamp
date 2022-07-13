1b263414da0c5290344dd9b13027a9843538452a
Jose Eduardo Rodriguez Fernandez <chelemovil_87@hotmail.es>
2022-07-13 19:59:14 +0200

    ejercicios 1,2 y 3

--- CursoReact/holamundo/src/App.js
+++ CursoReact/holamundo/src/App.js
@@ -1,20 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-// import Greeting from './components/pure/greeting'
-import GreetingF from './components/pure/greetingF';
-import TaskListComponent from './components/container/task_list';
+import logo from "./logo.svg";
+import "./App.css";
+// import GreetingF from './components/pure/greetingF';
+// import TaskListComponent from './components/container/task_list';
+import ComponenteA from "./components/componenteA";
 
 function App() {
+  const contactoPrueba = {
+    nombre: "Jose Eduardo",
+    apellido: "Rodriguez Fernandez",
+    email: "jose_rodriguez@open-bootcamp.com",
+    conectado: false,
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      {/* Componente propio Greeting.jsx */}
+        <ComponenteA contacto={contactoPrueba} />
+        {/* Componente propio Greeting.jsx */}
         {/* <Greeting name="Jose Eduardo"></Greeting> */}
         {/* Componente de ejemplo funcional */}
         {/* <GreetingF name="Jose Eduardo"></GreetingF> */}
         {/* Componente de llstado de tareas */}
-        <TaskListComponent></TaskListComponent>
+        {/* // <TaskListComponent></TaskListComponent> */}
       </header>
     </div>
   );
